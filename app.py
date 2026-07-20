ff474068c277fe41c140ae949caa2c48594513db
Norbert <n.gajderowicz@gmail.com>
2026-07-20 20:23:50 +0200

    feat: add new router

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
 from flask import Flask
 from api.routes import api
+from web.routes import web
 from monitor import start_monitor
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    static_folder="web/static",
+    template_folder="web/templates"
+    )
 
 app.register_blueprint(api)
+app.register_blueprint(web)
 
 start_monitor()
 
