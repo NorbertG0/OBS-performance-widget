fbf6887178e0304362efa4fadfbf5a1755fc9915
Norbert <n.gajderowicz@gmail.com>
2026-07-20 20:22:34 +0200

    feat: add getting information about size and speed ram modules

--- collectors/ram.py
+++ collectors/ram.py
@@ -1,4 +1,20 @@
 import psutil
+import wmi
+
+def get_ram_info():
+    c = wmi.WMI()
+    modules = []
+
+    for ram in c.Win32_PhysicalMemory():
+
+        modules.append({
+            "size": round(int(ram.Capacity) / 1024**3, 2),
+            "speed": ram.Speed
+        })
+
+    return modules
+
+ram_modules = get_ram_info()
 
 def get_ram():
     ram = psutil.virtual_memory()
@@ -6,5 +22,6 @@ def get_ram():
     return {
         "percent": ram.percent,
         "used": round(ram.used / 1024**3, 2),
-        "free": round(ram.available / 1024**3, 2)
+        "free": round(ram.available / 1024**3, 2),
+        "modules": ram_modules
     }
