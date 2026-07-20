679913a300ffb29968b690d5f0aee0f5f85675c4
Norbert <n.gajderowicz@gmail.com>
2026-07-20 19:36:04 +0200

    refactor: get functions from cpu and ram collectors

--- monitor.py
+++ monitor.py
@@ -5,28 +5,20 @@ import time
 from collectors.disk import get_disks
 from collectors.network import get_network
 from collectors.nvidia import get_nvidia
+from collectors.cpu import get_cpu
+from collectors.ram import get_ram
 
 from data.store import stats
 
 def monitor():
     while True:
 
-        cpu = psutil.cpu_percent(interval=1)
-        cpu_cores = psutil.cpu_percent(interval=None, percpu=True)
-
         ram = psutil.virtual_memory()
 
         stats.update({
-            "cpu": {
-                "percent": cpu,
-                "cores": cpu_cores
-            },
+            "cpu": get_cpu(),
 
-            "ram": {
-                "percent": ram.percent,
-                "used": round(ram.used / 1024**3, 2),
-                "free": round(ram.available / 1024**3, 2)
-            },
+            "ram": get_ram(),
 
             "disks": get_disks(),
 
